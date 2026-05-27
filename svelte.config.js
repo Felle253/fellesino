318b4ceaf3875068160dfd8e0ae796cfd651ccb2
Felle253 <felnor02@edu.umea.se>
2026-05-27 19:49:27 +0200

    fix: remove base path for root deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-		paths: {
+		/*paths: {
 			base: '/fellesino'
-		}
+		}*/
 	}
 };
 
